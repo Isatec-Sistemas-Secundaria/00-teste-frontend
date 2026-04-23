ebca85c64d2a0596086ccc48e4d9bf2627477896
Mateus Prado <maateussp@gmail.com>
2026-04-22 21:50:00 -0300

    feat: novos componentes e estilização para o formulario

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,24 @@
+interface InputProps {
+  text: string;
+  type?: string;
+  name?: string;
+  placeholder?: string;
+}
 
+export function Input({ text, type = "text", name, placeholder }: InputProps) {
+  return (
+    <div className="flex flex-col w-full max-w-[340px]">
+      <label className="mt-6 text-sm" htmlFor={name}>
+        {text}
+      </label>
 
-export function Input () {
-
-
-    return (
-        <>
-            <label htmlFor=""></label>
-            <input type="text" />
-        </>
-    )
+      <input
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        type={type}
+        className="h-10 w-full pl-4 rounded-md border border-gray-200  focus:border-[#2563EA] outline-none"
+      />
+    </div>
+  );
 }
